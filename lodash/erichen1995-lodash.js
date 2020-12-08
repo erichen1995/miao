7e074b971a6ecffd5427b89a8d29e1fd96b26d8b
erichen1995 <1366343126@qq.com>
2020-12-08 16:24:29 +0800

    测试chunk

--- lodash/erichen1995-lodash.js
+++ lodash/erichen1995-lodash.js
@@ -1,18 +1,17 @@
 var erichen1995 = function () {
-  // function chunk(array, size) {
-  //   let result = []
-  //   let m = 0
-  //   for (let i = 0; i < array.length; i++) {
-  //     result[m] = []
-  //     result[m].push(array[i])
-  //     if (result[m].length == size) {
-  //       m++
-  //     }
-  //   }
-  //   return result
-  // }
-
-
+  function chunk(array, size) {
+    let result = []
+    let m = 0
+    for (let i = 0; i < array.length; i++) {
+      result[m] ? result[m] : result[m] = [] 
+      result[m].push(array[i])
+      if (result[m].length == size) {
+        m++
+      }
+    }
+    return result
+  }
+  
   function compact(array) {
     let result = []
     for (let i = 0; i < array.length; i++) {
@@ -23,11 +22,8 @@ var erichen1995 = function () {
     return result
   }
 
-
-
-
   return {
     compact,
-    // chunk,
+    chunk,
   }
 }()
